8f5eb0aadc6003108de5613c30de7126419adbbb
Mthobisi Mkhonto <mmkhonto3@gmail.com>
2022-11-24 07:24:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Available Scripts
+
+git clone 
+
+# run npm install
+
+npm i
+
+# run project 
+
+npm start
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+// const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081/v1/teams"
+};
+
+app.use(cors({ origin: true }));
+
+app.use((req,res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin',"http://localhost:3000");
+  res.setHeader('Access-Control-Allow-Headers',"*");
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// parse requests of content-type - application/json
+app.use(express.json());  /* bodyParser.json() is deprecated */
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+
+const db = require("./app/models");
+db.sequelize.sync();
+// // drop the table if it already exists
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Test Working good" });
+});
+
+require("./app/routes/team.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
